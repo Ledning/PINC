8fbe7f54ed2c915ad4a0bfb5208ac522ab96891f
Mathias Leding <mathias@leding.dk>
2019-08-26 19:37:05 +0200

    working but now it is time to add pgp key error support

--- pinc.py
+++ pinc.py
@@ -2,16 +2,48 @@
 
 import argparse
 import subprocess
+from subprocess import check_output
 import requests
 from bs4 import BeautifulSoup
 import os
 import urllib
 import threading
+from enum import Enum
 
-packagepath = "/tmp/package"
+packagepath = "/home/usrname/package"
 makesettings = "-sir"
 threads = 10
 runlist = []
+
+
+class PkgVer(Enum):
+    newer = 0
+    uptodate = 1
+    outofdate = 2
+
+
+# def ver_comp(local, upstream):
+#     if local == upstream:
+#         return PkgVer.uptodate
+#     localpt = local.lower().replace("+", ".").replace("-", ".").replace(";", ".").split(".")
+#     upstreampt = upstream.lower().replace("+", ".").replace("-", ".").replace(";", ".").split(".")
+#
+#     while True:
+#         if len(localpt) == 0 and len(upstreampt) == 0:
+#             return PkgVer.uptodate
+#         elif len(localpt) == 0:
+#             return PkgVer.outofdate
+#         elif len(upstreampt) == 0:
+#             return PkgVer.newer
+#         localpop = localpt.pop()
+#         upstreampop = upstreampt.pop()
+#
+#         if localpop == upstreampop:
+#             continue
+#         if localpop < upstreampop:
+#             return PkgVer.outofdate
+#         if localpop > upstreampop:
+#             return PkgVer.newer
 
 
 def parser():
@@ -20,6 +52,7 @@ def parser():
     p.add_argument("-s", dest='search_flag', action='store_true', help='Search for package in the aur repository')
     p.add_argument("-u", dest='update_flag', action='store_true', help='Check for updates')
     p.add_argument("-r", dest='run_flag', action='store_true', help='Make packages')
+    p.add_argument('-v', dest='verbose_flag',action='store_true', help='Verbose output')
     p.add_argument('pkg', nargs='*', help='Package')
     return p, p.parse_args()
 
@@ -54,7 +87,7 @@ def main():
 
 
 def download_pkg(pkg):
-    if pkg is "":
+    if pkg == "":
         print("No package specified.")
         exit()
     try:
@@ -83,7 +116,7 @@ def search_pkg(query):
         for x in parsed_query[1:]:
             y = x.find('td', attrs={'class':'wrap'})
             print(x.a.contents[0] + ": " + str(y.text))
-        if len(parsed_query) is 0:
+        if len(parsed_query) == 0:
             print("No packages found")
     except:
         print("Could not search for package from aur.")
@@ -95,13 +128,13 @@ def update_pkg():
     package = str(subprocess_response.stdout.decode("utf-8")).split("\n")
     packagelist = []
     for pkg in package:
-        if pkg is not "":
+        if pkg != "":
             packagelist.append(pkg.split(" "))
     while len(packagelist) > 0:
         if threading.active_count()-1 < threads:
             pkg = packagelist.pop()
             threading.Thread(target=is_pkg_uptodate, args=(pkg[0], pkg[1], args.download_flag)).start()
-    while threading.active_count() != 1:
+    while args.download_flag and threading.active_count() != 1:
         pass
     return True
 
@@ -112,21 +145,35 @@ def is_pkg_uptodate(pkg, ver, download=False):
         response = requests.get(link)
         parsed_html = BeautifulSoup(response.text, "html.parser")
         newestpkg = str(parsed_html.find_all("h2")[1]).replace("<h2>Package Details: ", "").replace("</h2>", "").split(" ")
-        if newestpkg[1] is not ver:
+        if ver < newestpkg[1]:
             print("{} {} --> {}".format(pkg, ver, newestpkg[1]))
             if download:
                 download_pkg(pkg)
+        # elif ver_comp(ver, newestpkg[1]) == PkgVer.newer and args.verbose_flag:
+        #     print("\033[1;31mPINC :::     \033[0mLocal version is newer! " + ver + " | " + newestpkg[1])
 
-    except:
-        print("Could not fetch package from aur.")
+    except Exception as e:
+        print(e)
+        print("Could not fetch package from aur. ::: " + link)
         exit()
 
 
 def make_pkg(pkg):
     path = packagepath + "/" + pkg
     os.chdir(path)
-    p = subprocess.Popen(["makepkg", makesettings])
-    p.communicate()
+    try:
+        p = subprocess.Popen(["makepkg", makesettings])
+        # p.communicate()
+        p.communicate()
+        # TODO: Run this gpg --search-keys A2C794A986419D8A
+        # for line in p.stdout.readlines():
+        #     print("A line")
+        #     if "public key" in line:
+        #         print("Oh my god we did it")
+
+    except:
+        if args.verbose_flag:
+            print("\033[1;31mPINC :::     \033[0mFailed to build pkg")
 
 
 if __name__ == "__main__":
